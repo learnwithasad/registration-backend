a154e34371bcdbdcd35c09c92a4f5639d9daab10
Muhammad Asad <asad757622@gmail.com>
2025-08-09 10:35:40 +0500

    Update coupon.model.js

--- models/coupon.model.js
+++ models/coupon.model.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const couponSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    coupon: { type: String, required: true },
+    coupon: { type: String, required: true, trim: true,
+    lowercase: true },
     password: { type: String, required: true },
   },
   { timestamps: true }
